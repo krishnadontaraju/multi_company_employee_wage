df1b8a7bfcd79ae010c33543e1b72090d04b3107
Sai Krishna <reachkrishnadontaraju@gmail.com>
2021-03-26 08:03:42 +0530

    computed the wage within conditions

--- emp_wage_OOP.java
+++ emp_wage_OOP.java
@@ -12,8 +12,9 @@ public class employee_wage_OOP {
 
 
         public static class Employee_check {
-            // Initializing working conditions
+
             int working_hours = 8;
+            int work_days = 0; 
             int absent_counter = 0;
             int part_time_counter = 0;
             int full_time_counter = 0;
@@ -22,35 +23,35 @@ public class employee_wage_OOP {
             int part_time_hrs = 4;
             int wage_today = 0;
             int total_work_hrs = 0;
-            
-    
+                
             Random check = new Random();
             //Defining attendacne method
             public void attendance() {
-                      
-    
+                
                 //int checker =check.nextInt(3);
 
-                for(int i =0 ; i<30; i++){
+                while(total_work_hrs < 100 && work_days < 20 ){
 
                     int checker =check.nextInt(3);
-
+                    // Using switch to mark attendance
                     switch(checker){
-                        //Absent case
+                        // Absent case
                         case 0:
                             absent_counter += 1;
                             break;
-                        // Full time case    
+                        // Full time Case    
                         case 1:
                             wage_today = working_hours * wage_per_hr;
                             total_work_hrs += 8;
                             full_time_counter += 1;
+                            work_days += 1;
                             break;
-                        // Part time case    
+                        // Part time Case    
                         case 2 :
                             wage_today = working_hours * wage_per_hr;
                             total_work_hrs += 4;
                             part_time_counter += 1;
+                            work_days += 1;
                             break;
                         default:
                             break;            
@@ -60,6 +61,7 @@ public class employee_wage_OOP {
              }
 
              int wage_total = total_work_hrs * wage_per_hr;
+             System.out.println("THE EMPLOYE WORKED FOR "+work_days+" DAYS AND "+total_work_hrs+" HOURS");
              System.out.println("THE EMPLOYE WAS ABSENT FOR "+absent_counter+" DAYS");
              System.out.println("THE EMPLOYEE WORKED "+part_time_counter+" PART TIME DAYS");
              System.out.println("THE EMPLOYEE WORKED "+full_time_counter+" FULL TIME DAYS");
@@ -68,9 +70,5 @@ public class employee_wage_OOP {
             } 
             
         }
-        
-    }
-      
-
-
     
+}
