9274442113caefb9024d8cf41f0a81b602c1d811
Sai Krishna <reachkrishnadontaraju@gmail.com>
2021-03-26 07:55:28 +0530

    used switch case to compute the daily wage

--- emp_wage_OOP.java
+++ emp_wage_OOP.java
@@ -1,7 +1,6 @@
 import java.util.Random;
 
-public class employee_wage_OOP {
-
+public class employee_wage_UC_4 {
 
     public static void main(String[] args) {
         // Creating an Instance of class Employee_check
@@ -15,28 +14,33 @@ public class employee_wage_OOP {
     //Defining the class Employee_check
     public static class Employee_check {
 
-        Employee_check(/*int i, int j*/){
-            //Initializing the wage and working hours
-            int working_hours = 8;
-            int wage_per_hr = 20;
-            //Initializing part time hours
-            int part_time_hrs = 4;
-
-        }
+        int working_hours = 8;
+        int wage_per_hr = 20;
+        //Initializing part time hours
+        int part_time_hrs = 4;
+        int wage_today = 0;
 
         Random check = new Random();
         //Defining attendacne method
         public void attendance() {
 
-            
-
-            int checker =check.nextInt(2);
+            int checker =check.nextInt(3);
             //Marking the attendance of the employee
-            if (checker == 1)
-                System.out.println("THE EMPLOYEE IS PRESENT");
-            else
-                System.out.println("THE EMPLOYEE IS ABSENT");    
-            
+            switch(checker){
+                case 0:
+                    System.out.println("THE EMPLOYEE IS ABSENT,Wage for today is 0");
+                    break;
+                case 1:
+                    wage_today = working_hours * wage_per_hr;
+                    System.out.println("THE EMPLOYEE HAS DONE A FULL AND WAGE TODAY IS "+wage_today);
+                    break;
+                case 2 :
+                    wage_today = working_hours * wage_per_hr;
+                    System.out.println("THE EMPLOYE HAS DONE PART TIME AND THE WAGE FOR TODAY IS "+wage_today);
+                    break;
+                default:
+                    break;            
+            }
         }
 
     }
